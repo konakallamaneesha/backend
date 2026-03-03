259a7d59c6c67204899adfe0e558d9de871ac01e
konakallamaneesha <konakallamaneesha13@gmail.com>
2026-03-03 11:34:32 +0530

    changes

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const passwordRoutes = require("./routes/passwordRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://securevaultfrontend.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes first load
@@ -23,11 +26,11 @@ app.get("/", (req, res) => {
 // Server start first
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
-// DB connect later
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log("MongoDB Error:", err.message));
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => console.log("MongoDB Error:", err.message));
